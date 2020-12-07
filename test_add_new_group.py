3be5cdc6b306900235c96066ef489c70a1f53989
pavelkalinchuk <715m4s@gmail.com>
2020-12-08 00:11:19 +0300

    Немного подправил чистоту кода (PEP8)

--- test_add_new_group.py
+++ test_add_new_group.py
@@ -11,7 +11,6 @@ class TestAddNewGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
     def test_add_new_group(self):
         wd = self.wd
         wd.get("https://localhost/addressbook/")
@@ -34,20 +33,17 @@ class TestAddNewGroup(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
     
-
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
-
     @property
     def is_alert_present(self):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
     
-
     def tearDown(self):
         self.wd.quit()
 
