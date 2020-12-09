02777eddf6f06972cd63258fb73de23edcfd4c74
pavelkalinchuk <715m4s@gmail.com>
2020-12-09 19:55:05 +0300

    Добавления универсальности для теста (выполнено преобразование методов авторизации и создания группы, входные данные этих методов передаются "явным" образом в коде и могут быть изменены на любые значения)

--- test_add_new_group.py
+++ test_add_new_group.py
@@ -14,9 +14,19 @@ class TestAddNewGroup(unittest.TestCase):
     def test_add_new_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_page_with_groups(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="test", header="test", footer="test")
+        self.return_groups_page(wd)
+        self.logout(wd)
+
+
+    def test_add_new_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_page_with_groups(wd)
+        self.create_group(wd, name="empty", header="empty", footer="empty")
         self.return_groups_page(wd)
         self.logout(wd)
 
@@ -26,29 +36,29 @@ class TestAddNewGroup(unittest.TestCase):
     def return_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # Creating new group
         wd.find_element_by_name("new").click()
         # Fill group firm
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("test")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("test")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # Submit group creation
         wd.find_element_by_name("submit").click()
 
     def open_page_with_groups(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
