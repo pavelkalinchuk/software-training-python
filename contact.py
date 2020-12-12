a174caaa98aaa35b7ae976d3a700e364c4883c1b
pavelkalinchuk <715m4s@gmail.com>
2020-12-12 17:29:39 +0300

    Завершена работа по третьему тестовому заданию На тестовом сайте с адресной книжкой добавляется новый контакт с параметрами ФИО, адрес, е-мэйл, мобильный телефон

--- contact.py
+++ contact.py
@@ -1,5 +1,8 @@
 class Contact:
-    def __init__(self, first_name, last_name, middle_name):
+    def __init__(self, first_name, last_name, middle_name, address, email, phone_mobile):
         self.first_name = first_name
         self.last_name = last_name
         self.middle_name = middle_name
+        self.address = address
+        self.email = email
+        self.phone_mobile = phone_mobile
--- test_add_new_contact.py
+++ test_add_new_contact.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from contact import Contact
 import unittest
 import time
 
@@ -13,7 +14,7 @@ class TestAddNewContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.authorization(wd, username="admin", password="secret")
-        self.add_new_contact(wd, first_name="first_name", last_name="last_name")
+        self.add_new_contact(wd, Contact(first_name="first_name", last_name="last_name", middle_name="middle_name", address="Москва, ул. Тестовая, 1", email="test@mail.com", phone_mobile="+79161234567"))
         self.return_home_page(wd)
         self.logout(wd)
 
@@ -28,14 +29,26 @@ class TestAddNewContact(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def add_new_contact(self, wd, first_name, last_name):
+    def add_new_contact(self, wd, contact):
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(first_name)
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(last_name)
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.phone_mobile)
         wd.find_element_by_name("submit").click()
 
     def return_home_page(self, wd):
