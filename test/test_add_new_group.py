318cb2dbd8c9728b10473de93c21c91e3e41fe4f
pavelkalinchuk <715m4s@gmail.com>
2020-12-16 22:41:23 +0300

    Переименован файл фикстуры через рефакторинг

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from appgroup import Application
+from model.group import Group
+from fixture.appgroup import Application
 
 
 @pytest .fixture
@@ -13,7 +13,10 @@ def app(request):
 
 def test_add_new_group(app):
     app.login(username="admin", password="secret")
-    app.create_group(Group(name="automate name", header="automate header", footer="automate footer"))
+    app.create_group(Group(
+        name="automate name",
+        header="automate header",
+        footer="automate footer"))
     app.logout()
 
 
