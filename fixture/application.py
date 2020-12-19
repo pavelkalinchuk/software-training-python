4deb335fa0101dda2cbbef28f10bb818da852e8f
pavelkalinchuk <715m4s@gmail.com>
2020-12-19 13:51:44 +0300

    Тесты по группам и контактам теперь используют общую фикстуру application.py (appcontact.py упразднен) и один session.py (session_contact.py упразднен).

--- fixture/application.py
+++ fixture/application.py
@@ -1,19 +1,21 @@
 from selenium import webdriver
-from fixture.session_contact import SessionContHelper
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 
-class Appcontact:
+class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-        self.session_contact = SessionContHelper(self)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("https://localhost/addressbook/")
+        wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.appcontact import Appcontact
 
 
-@pytest.fixture
-def appcontact(request):
-    fixture = Appcontact()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-
-def test_add_new_contact(appcontact):
-    appcontact.session_contact.authorization(username="admin", password="secret")
-    appcontact.contact.add_new_contact(Contact(
+def test_add_new_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.add_new_contact(Contact(
         first_name="Иван",
         last_name="Иванов",
         middle_name="Иванович",
         address="Москва, ул. Тестовая, 1",
         email="test@mail.com",
         phone_mobile="+79161234567"))
-    appcontact.session_contact.logout()
+    app.session.logout()
