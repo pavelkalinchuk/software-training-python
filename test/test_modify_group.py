1e7dcd20292d9ff796bda732c71543e69793d363
pavelkalinchuk <715m4s@gmail.com>
2020-12-26 22:43:23 +0300

    Реализована проверка на наличие групп в списке групп при тесте на изменение данных первой из списка группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,4 +3,6 @@ from model.group import Group
 
 
 def test_modify_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test_for_delete_group"))
     app.group.modify_group(Group(name="modify_name"))
