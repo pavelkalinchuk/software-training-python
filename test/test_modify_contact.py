87e41d6cfb760576fe0fb8462caa41ea36c455c2
pavelkalinchuk <715m4s@gmail.com>
2020-12-26 23:28:08 +0300

    Реализована проверка на наличие контактов в списке контактов при тесте на изменение первого контакта из списка контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,6 +3,12 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
+    if app.contact.count() == 0:
+        app.contact.add_new_contact(Contact(
+            first_name="Для Изменения",
+            last_name="Контакт",
+            email="modify@mail.com"))
+
     app.contact.modify_contact(Contact(
         first_name="Иван_modify",
         last_name="Иванов_modify",
