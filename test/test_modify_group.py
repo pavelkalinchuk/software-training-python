6371b451ffb179b2d7794a6a36fcdcb32812f1b2
pavelkalinchuk <715m4s@gmail.com>
2021-01-06 17:11:39 +0300

    В тест модификации группы добавлена альтернативная проверка списка групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -6,6 +6,10 @@ def test_modify_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test_for_delete_group"))
     old_groups = app.group.get_group_list()
-    app.group.modify_group(Group(name="modify_name"))
+    group = Group(name="modify_name")
+    group.id = old_groups[0].id
+    app.group.modify_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
