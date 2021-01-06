554058c1291a6dfa42761d3ac4dcc0722bc9c1fc
pavelkalinchuk <715m4s@gmail.com>
2021-01-06 15:39:29 +0300

    Реализован метод  get_group_list для построения списка груп Добавлена переменная old_groups в которую помещаем список груп до добавления Добавлена переменная new_groups в которую помещаем список груп после добавления И производим сравнение длин полученных списков груп

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -13,4 +13,7 @@ def test_add_new_group(app):
 
 
 def test_add_new_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
