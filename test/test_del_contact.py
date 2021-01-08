c3fd81382f80ba8d02daf37209f8b73ed58f348a
pavelkalinchuk <715m4s@gmail.com>
2021-01-08 14:50:17 +0300

    Добавлены проверки сравнением длин списков контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -8,4 +8,9 @@ def test_delete_first_contact(app):
             first_name="Для Удаления",
             last_name="Контакт",
             email="delete@mail.com"))
+    old_contact = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contact = app.contact.get_contact_list()
+    assert len(old_contact) - 1 == len(new_contact)
+    old_contact[0:1] = []
+    assert old_contact == new_contact
