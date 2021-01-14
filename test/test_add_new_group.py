76e766fe3b21e0434159a63daf942822ae3c7484
pavelkalinchuk <715m4s@gmail.com>
2021-01-15 00:55:37 +0300

    Добавлена параметризация для теста добавления групп

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import string
+import random
 
 
-def test_add_new_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdate = [
+               Group(name=random_string("name", 10),
+                     header=random_string("header", 20),
+                     footer=random_string("footer", 10))
+               for i in range(3)
+           ] + [Group(name="", header="", footer="")]
+
+
+@pytest.mark.parametrize("group", testdate, ids=[repr(x) for x in testdate])
+def test_add_new_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(
-        name="automate name",
-        header="automate header",
-        footer="automate footer")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_add_new_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
     app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
