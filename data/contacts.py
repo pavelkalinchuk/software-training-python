5ba3c099d82729414e16d4339de01ac621f2e51b
pavelkalinchuk <715m4s@gmail.com>
2021-01-23 17:21:37 +0300

    Фикстура указывающая на источник данных  (для контактов)

---
+++ data/contacts.py
@@ -0,0 +1,14 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(
+        first_name="Иван",
+        last_name="Иванов",
+        middle_name="Иванович",
+        address="Москва, ул. Строителей, 1",
+        email="ivan@mail.ru",
+        phone_mobile="+7(913)123-23-67",
+        phone_home="+7(495)781-22-09",
+        phone_work="+7(495)123-20-00",
+        phone_secondary="+7(800)100-00-00")
+]
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-from data.add_contact import constant as testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_man_contact(app, contact):
+def test_add_man_contact(app, data_contacts):
     old_contacts = app.contact.get_contact_list()
-    app.contact.add_new_contact(contact)
+    app.contact.add_new_contact(data_contacts)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
-    old_contacts.append(contact)
+    old_contacts.append(data_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
