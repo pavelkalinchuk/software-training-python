655239038fd595284fbd50cbfdd240578821da18
pavelkalinchuk <715m4s@gmail.com>
2021-01-23 17:09:39 +0300

    Фикстура указывающая на источник данных  (для груп)

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-from data.add_group import constant as testdata
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_new_group(app, group):
+def test_add_new_group(app, data_groups):
     old_groups = app.group.get_group_list()
-    app.group.create(group)
+    app.group.create(data_groups)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
+    old_groups.append(data_groups)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
