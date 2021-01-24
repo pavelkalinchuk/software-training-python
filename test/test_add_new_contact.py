b1598232d3681d2af1208f076c8621a51c044550
pavelkalinchuk <715m4s@gmail.com>
2021-01-24 13:30:17 +0300

    Реализовано получение тестовых данных из json-файла (контакты)

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -2,10 +2,14 @@
 from model.contact import Contact
 
 
-def test_add_man_contact(app, data_contacts):
+def test_add_man_contact(app,
+                         json_contacts):  # для выбора источника тестовых данных указать или "data_groups" или "json_groups"
+    contacts = json_contacts
     old_contacts = app.contact.get_contact_list()
-    app.contact.add_new_contact(data_contacts)
+    app.contact.add_new_contact(contacts)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
-    old_contacts.append(data_contacts)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    old_contacts.append(contacts)
+    assert sorted(
+        old_contacts, key=Contact.id_or_max) == sorted(
+        new_contacts, key=Contact.id_or_max)
